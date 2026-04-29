0e209dbaf2319a3016a449901d9df5ad77d1c3c2
Ankur Paudel <ankur@paudel.me>
2026-04-29 14:21:28 +0100

    test: add tests for result backend

---
+++ tests/integration/test_result_backend.py
@@ -0,0 +1,87 @@
+import secrets
+
+import pytest
+import sqlalchemy as sa
+from sqlalchemy.ext.asyncio import AsyncEngine
+from taskiq import AsyncBroker, InMemoryBroker
+from taskiq.exceptions import ResultGetError
+from taskiq_sqlalchemy.result_backend import SQLAlchemyResultBackend
+
+
+async def test_when_startup_called__then_table_created(db_engine: AsyncEngine):
+    table_name = secrets.token_hex()
+    backend = SQLAlchemyResultBackend(db_engine, table_name=table_name)
+
+    # get list of tables
+    async with db_engine.connect() as conn:
+        meta = sa.MetaData()
+
+        # check table does not already exist
+        await conn.run_sync(meta.reflect)
+        assert meta.tables.get(table_name) is None
+
+        # WHEN: startup called
+        await backend.startup()
+
+        # THEN: table created
+        await conn.run_sync(meta.reflect)
+        assert meta.tables.get(table_name) is not None
+
+
+async def test_when_task_is_processed__then_result_is_stored(broker: AsyncBroker):
+    val = secrets.token_hex()
+
+    @broker.task
+    async def _test_task():
+        return val
+
+    # WHEN: task is processed
+    kicker = await _test_task.kiq()
+    print("kicker sent")
+    result = await kicker.wait_result()
+    print("result got")
+
+    # THEN: result is stored
+    assert not result.is_err
+    assert result.return_value == val
+
+
+async def test_given_keep_results_false__when_result_is_accessed__then_result_is_removed(
+    db_engine,
+):
+    # GIVEN: keep_results = False
+    result_backend = SQLAlchemyResultBackend(db_engine, keep_results=False)
+    broker = InMemoryBroker().with_result_backend(result_backend)
+    await result_backend.startup()
+
+    @broker.task
+    def _test_task():
+        return 1
+
+    kicker = await _test_task.kiq()
+
+    # WHEN: result is accessed
+    result = await kicker.wait_result()
+    assert not result.is_err
+
+    # THEN: result should be deleted
+    with pytest.raises(ResultGetError):
+        _result2 = await kicker.get_result()
+
+
+async def test_given_keep_results_true__when_result_is_accessed__then_result_is_kept(
+    broker: AsyncBroker,
+):
+    # GIVEN: keep_results = True
+    @broker.task
+    def _test_task():
+        return 1
+
+    kicker = await _test_task.kiq()
+    # WHEN: result is accessed
+    result = await kicker.wait_result()
+    assert not result.is_err
+
+    # THEN: result should not be deleted
+    result2 = await kicker.get_result()
+    assert not result2.is_err
